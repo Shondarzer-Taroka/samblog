5852a44847fe30a3572d409a376f5bb7d2c1eb11
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-06-30 14:58:52 +0600

    faces some issues

--- src/swiper/PhotoSlider.tsx
+++ src/swiper/PhotoSlider.tsx
@@ -74,9 +74,7 @@ const PhotoSlider: React.FC = () => {
     <div className="bg-white  overflow-hidden relative py-6 px-2  md:px-4">
       {/* <h2 className="text-xl font-semibold text-red-600 mb-4 py-2.5">  গ্যালারি</h2> */}
 
-<div className="mb-8">
-  <TitleNewsOverSection headline="গ্যালারি"/>
-</div>
+
       {/* Progress bar */}
       <div
         key={`${current}-${isPaused}`}  // forces re-render to restart animation
