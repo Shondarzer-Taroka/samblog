64d90394dfe2dc40b3a72c3dbbc409c18ea3fab5
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-06-30 11:10:16 +0600

    mb-8 added in div

--- src/components/HomePages/Entertainment/Entertainment.tsx
+++ src/components/HomePages/Entertainment/Entertainment.tsx
@@ -5,7 +5,7 @@ import TitleNewsOverSection from '@/share/TitleNewsOverSection';
 const Entertainment = () => {
     return (
         <div className=' px-2 md:px-4 py-6'>
-            <div>
+            <div className='mb-8'>
                 <TitleNewsOverSection headline='বিনোদন'/>
             </div>
             {/* <h1 className="text-xl font-semibold text-red-600 mb-4">বিনোদন</h1> */}
