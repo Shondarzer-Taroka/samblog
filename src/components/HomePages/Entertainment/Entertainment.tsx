c410ca099eb26d790dec209f55f61be71b5d5f68
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-06-30 11:07:43 +0600

    title oversection import

--- src/components/HomePages/Entertainment/Entertainment.tsx
+++ src/components/HomePages/Entertainment/Entertainment.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import NewsGrid from './NewsGrid';
+import TitleNewsOverSection from '@/share/TitleNewsOverSection';
 
 const Entertainment = () => {
     return (
         <div className=' px-2 md:px-4 py-6'>
             <div>
-                
+                <TitleNewsOverSection/>
             </div>
             <h1 className="text-xl font-semibold text-red-600 mb-4">বিনোদন</h1>
             <NewsGrid/>
