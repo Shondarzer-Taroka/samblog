9ad29026323893489fdffa04ebec65d82c7d79e5
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-06-30 11:08:50 +0600

    headline added

--- src/components/HomePages/Entertainment/Entertainment.tsx
+++ src/components/HomePages/Entertainment/Entertainment.tsx
@@ -6,7 +6,7 @@ const Entertainment = () => {
     return (
         <div className=' px-2 md:px-4 py-6'>
             <div>
-                <TitleNewsOverSection headline=''/>
+                <TitleNewsOverSection headline='বিনোদন'/>
             </div>
             <h1 className="text-xl font-semibold text-red-600 mb-4">বিনোদন</h1>
             <NewsGrid/>
