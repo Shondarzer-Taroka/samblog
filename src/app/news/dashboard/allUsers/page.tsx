1cc01998f168c4807720e17d81c53232d22e0033
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-13 19:00:17 +0600

    all users getting

--- src/app/news/dashboard/allUsers/page.tsx
+++ src/app/news/dashboard/allUsers/page.tsx
@@ -1,9 +1,10 @@
+import Users from '@/components/Dashboard/Users';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            
+            <Users/>
         </div>
     );
 };
