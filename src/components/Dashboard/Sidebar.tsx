4dd5059e22dbb2a92a8639ed5b69b5047fbd0dff
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-14 14:31:51 +0600

    hanlde togge function created

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -68,6 +68,7 @@ import Link from 'next/link';
 import { ClipboardPen, Newspaper, UserRoundPen } from 'lucide-react';
 import { useAuthProvider } from '@/Providers/AuthProvider';
 import { FaClosedCaptioning } from 'react-icons/fa';
+import { useState } from 'react';
 
 const allLinks = [
     { label: 'মূলপাতা', icon: <FiHome />, href: '/', roles: ['admin', 'editor', 'user'] },
@@ -82,6 +83,11 @@ const allLinks = [
 
 export default function Sidebar({ isOpen }: { isOpen: boolean }) {
     const { loading, user } = useAuthProvider();
+    const [toggle, setToggle] = useState<boolean>(true)
+
+    function handleToggle() {
+        setToggle(!toggle)
+    }
 
     if (loading) {
         return <div className='bg-gray-900 text-white w-64 space-y-4 px-4 py-6 
@@ -101,11 +107,12 @@ export default function Sidebar({ isOpen }: { isOpen: boolean }) {
             className={`bg-gray-900 text-white w-64 space-y-4 px-4 py-6 
         fixed md:sticky  md:top-0 h-screen z-50 
         transform transition-transform duration-300 
+        
         ${isOpen ? 'translate-x-0' : '-translate-x-full md:translate-x-0'}`}
         >
 
-            <div>
-                <FaClosedCaptioning />
+            <div className='absolute right-1 top-0'>
+                <FaClosedCaptioning className='text-white ' />
             </div>
 
             <h2 className="text-2xl font-bold mb-6">ড্যাশবোর্ড</h2>
