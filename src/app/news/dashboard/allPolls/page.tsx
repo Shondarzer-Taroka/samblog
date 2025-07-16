ec34d9238dc2f81c30104fe23eea7ba50807b105
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-16 19:19:49 +0600

    home page getnewsfunction modified

---
+++ src/app/news/dashboard/allPolls/page.tsx
@@ -0,0 +1,12 @@
+import AllPolls from '@/components/Dashboard/AllPolls';
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <AllPolls/>
+        </div>
+    );
+};
+
+export default page;
