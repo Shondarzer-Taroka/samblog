494d717c58d480156b6c279ff8f604264b70be80
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-23 14:45:03 +0600

    socket context created

--- src/app/news/dashboard/layout.tsx
+++ src/app/news/dashboard/layout.tsx
@@ -1,54 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// 'use client';
-
-// import Sidebar from '@/components/Dashboard/Sidebar';
-// import Topbar from '@/components/Dashboard/Topbar';
-// import AuthMiddleware from '@/middleware/AuthMiddleware';
-// import { useAuthProvider } from '@/Providers/AuthProvider';
-// // import RouteProtector from '@/components/RouteProtector';
-// import { useState } from 'react';
-
-
-
-// export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-//   const [isSidebarOpen, setSidebarOpen] = useState(true);
-//   const { user, loading } = useAuthProvider()
-
-// if (loading) {
-//   return  <p className="text-center py-10">লোড হচ্ছে...</p>
-// }
-//   return (
-//     //  <RouteProtector>
-//     <AuthMiddleware>
-//       {user?.email && <div className="flex font-noto">
-//         <Sidebar isOpen={isSidebarOpen} />
-//         <div className={`flex-1 transition-all duration-300 ${isSidebarOpen ? 'md:ml-6' : 'md:ml-0'}`}>
-//           <Topbar toggleSidebar={() => setSidebarOpen(prev => !prev)} isSidebarOpen={isSidebarOpen} />
-//           <main className="p-4 bg-gray-100 min-h-screen">{children}</main>
-//         </div>
-//       </div>}
-//     </AuthMiddleware>
-//   );
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // app/news/dashboard/layout.tsx
 'use client';
 
 import Sidebar from '@/components/Dashboard/Sidebar';
