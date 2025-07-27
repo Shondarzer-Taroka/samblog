af00bac8075817e619ce49766e19b4d1c9688087
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-27 13:04:03 +0600

    server done

---
+++ src/components/Dashboard/DashboardOverviewPage.tsx
@@ -0,0 +1,29 @@
+// import { fetchDashboardData, fetchHistoricalData } from "@/lib/api/dashboard-api";
+// import DashboardClient from "./SessionsOverviewChart";
+
+
+// export default async function DashboardOverviewPage() {
+//   const [dashboardData, historicalData] = await Promise.all([
+//     fetchDashboardData(),
+//     fetchHistoricalData()
+//   ]);
+
+//   return (
+//     <DashboardClient 
+//       dashboardData={dashboardData} 
+//       historicalData={historicalData} 
+//     />
+//   );
+// }
+
+import React from 'react';
+
+const DashboardOverviewPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default DashboardOverviewPage;
