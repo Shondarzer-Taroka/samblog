baa9911934953669be9e3191be43c50f2d3a8b45
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-31 17:56:29 +0600

    my opinions starts

---
+++ src/app/news/dashboard/myOpinions/page.tsx
@@ -0,0 +1,15 @@
+// 'use client'
+// import { useAuthProvider } from '@/Providers/AuthProvider';
+// import React from 'react';
+
+// const page = () => {
+//     const { user, loading } = useAuthProvider()
+//     return (
+//         <div>
+
+//         </div>
+//     );
+// };
+
+// export default page;
+
