546d5aa3cacce95ea0bae0931c453e2a7a0277fa
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-01 12:21:56 +0600

    loading added

---
+++ src/app/news/[category]/[id]/loading.tsx
@@ -0,0 +1,12 @@
+import Loading from '@/app/loading';
+import React from 'react';
+
+const loading = () => {
+    return (
+        <div>
+            <Loading/>
+        </div>
+    );
+};
+
+export default loading;
