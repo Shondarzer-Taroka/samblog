9bc002a79610cbde6f447e3dd918762268f705d0
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-06 17:34:39 +0600

    area filter searching button width in sm  devices full and auto in md devices

--- src/components/HomePages/AreaNewsFilter/AreaNewsFilter.tsx
+++ src/components/HomePages/AreaNewsFilter/AreaNewsFilter.tsx
@@ -226,7 +226,7 @@ const AreaNewsFilter = ({ onSearch, isHomePage = false }: AreaNewsFilterProps) =
         </div>
 
         <button 
-          className="bg-blue-600 hover:bg-blue-700 text-white px-5 py-[9.5px] rounded-[4px] flex items-center justify-center gap-2 text-sm whitespace-nowrap shadow-md hover:shadow-lg transition-all duration-300 disabled:bg-blue-400 disabled:cursor-not-allowed min-w-[100px]"
+          className=" w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white px-5 py-[9.5px] rounded-[4px] flex items-center justify-center gap-2 text-sm whitespace-nowrap shadow-md hover:shadow-lg transition-all duration-300 disabled:bg-blue-400 disabled:cursor-not-allowed min-w-[100px]"
           onClick={handleSearch}
           disabled={isLoading || !division}
         >
